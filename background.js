61490c389a6d36d593e3c5ed17da0b99a07136b5
liuminghao01 <donymh@163.com>
2021-12-15 22:57:38 +0800

    init

---
+++ background.js
@@ -0,0 +1,7 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: liuminghao@benlai.com
+ * @Date: 2021-12-15 22:30:05
+ * @LastEditTime: 2021-12-15 22:30:05
+ */
---
+++ popup.js
@@ -0,0 +1,7 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: liuminghao@benlai.com
+ * @Date: 2021-12-15 22:29:50
+ * @LastEditTime: 2021-12-15 22:29:50
+ */
