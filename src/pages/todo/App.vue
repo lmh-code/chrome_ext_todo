daf867bf6f9d695e1eb049fef417bd0d34335302
donymh <donymh@163.com>
2021-12-28 22:58:47 +0800

    feat: popup页面引入 v-calendar 日历视图组件

--- src/pages/todo/App.vue
+++ src/pages/todo/App.vue
@@ -22,13 +22,24 @@ export default {
 <style scoped lang="scss">
   .todo-wrap {
     width: 100%;
+    height: 100vh;
     display: flex;
+    padding: 20px 10%;
+    box-sizing: border-box;
+    overflow: hidden;
+    background-color: $-color-main-grey;
     .todo-wrap__left {
-      width: 40%;
+      width: 60%;
       border-right: 1px solid #ececec;
+      background-color: $-color-white;
+      border-top-left-radius: 16px;
+      border-bottom-left-radius: 16px;
     }
     .todo-wrap__right {
       flex: 1;
+      background-color: $-color-white;
+      border-top-right-radius: 16px;
+      border-bottom-right-radius: 16px;
     }
   }
 </style>
--- src/pages/todo/components/TimePicker.vue
+++ src/pages/todo/components/TimePicker.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    日期控件
+  <div style="height: 100px; width: 100px;">
+    <v-calendar is-expanded />
   </div>
 </template>
 
 <script>
+import VCalendar from 'v-calendar'
 export default {
-
+  components: {
+    VCalendar
+  }
 }
 </script>
 
